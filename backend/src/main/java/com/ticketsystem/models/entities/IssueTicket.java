839f9ca973a8f4a9fa68f0cba05779b7c0a3de94
orozb <orozbekova.kyzzhjibek@gmail.com>
2025-10-24 15:14:48 +0200

    add IssueTicket.java entity

---
+++ backend/src/main/java/com/ticketsystem/models/entities/IssueTicket.java
@@ -0,0 +1,62 @@
+package com.ticketsystem.models.entities;
+
+import com.ticketsystem.models.enums.Priority;
+import com.ticketsystem.models.enums.Status;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "issue")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class IssueTicket extends BaseEntity {
+
+    @Column(columnDefinition = "TEXT")
+    String description;
+
+    @Column(length = 100)
+    String city;
+
+    @Column(length = 50)
+    String apartmentNumber;
+
+    @Column(length = 50)
+    String roomNumber;
+
+    @Column(name = "image_url")
+    String imageUrl;
+
+    @Column(name = "authorization_accepted")
+    Boolean authorizationAccepted;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    Priority priority;
+
+    @Column(name = "current_status", length = 50, nullable = false)
+    @Enumerated(EnumType.STRING)
+    Status currentStatus;
+
+    @Column(name = "resolved_at")
+    LocalDateTime resolvedAt;
+
+    @Column(name = "external_company_name")
+    String externalCompanyName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    User createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assigned_to")
+    User assignedTo;
+
+}
