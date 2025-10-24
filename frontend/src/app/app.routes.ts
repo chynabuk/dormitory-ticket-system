3a1a07e17b67638c1a574a35ea69c3a78c0c5f66
Kubanychbek Kushtarbekov <Kubanychbek.Kushtarbekov.ne9@fh-zwickau.de>
2025-10-24 18:19:55 +0200

    Add login form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: DashboardComponent
+    },
+    {
+        path: 'sign-out',
+        redirectTo: '/sign-in'
+    },
+    {
+        path: 'sign-in',
+        component: LoginFormComponent
     }
 ];
---
+++ frontend/src/app/login-form/login-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css'
+})
+export class LoginFormComponent {
+  public email = '';
+  public password = '';
+}
