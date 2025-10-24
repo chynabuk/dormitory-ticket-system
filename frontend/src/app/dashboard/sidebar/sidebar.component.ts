4055b01eb0f59fca0a50b18cc7ebe881cd59c0c9
Kubanychbek Kushtarbekov <Kubanychbek.Kushtarbekov.ne9@fh-zwickau.de>
2025-10-24 17:57:02 +0200

    Add sidebar

---
+++ frontend/src/app/dashboard/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  public sidebarItems = [
+    {
+      label: 'Tickets',
+      route: ''
+    },
+    {
+      label: 'Users',
+      route: ''
+    }
+  ]
+}
