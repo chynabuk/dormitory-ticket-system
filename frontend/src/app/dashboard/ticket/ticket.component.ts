bfa3567e4e5b17ba4726426a611e0dfd4738c494
Alaken Kubatbekov <Alaken.Kubatbekov.n49@fh-zwickau.de>
2025-10-25 03:39:15 +0200

    just commit

--- frontend/src/app/dashboard/ticket/ticket.component.ts
+++ frontend/src/app/dashboard/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  public htmlToPDF() {
+    let element = document.getElementById('paperToPdf');
+    if (element) {
+      element.classList.add("border-bottom")
+      element.classList.add("border-3")
+      const options = {
+        filename: 'ticket.pdf',
+        jsPDF: {
+          unit: 'mm',
+          format: 'a4',
+          orientation: 'portrait' as 'portrait',
+        },
+      };
 
+      html2pdf().set(options).from(element).save();
+    }
+  }
 }
